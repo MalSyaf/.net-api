1e5c45546411f736fb1b4a922f9f4efcbddf942b
MalSyaf <syafqz@gmail.com>
2024-01-26 12:14:18 +0800

    Controller setup

--- obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+330272a373655773add23f5105516e6e50849dd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5236c28d5973ab598bc4ad4934c0398bb2438a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
