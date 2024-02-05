85f54058f71128513f3bd3d7c18fac9b0ed1f79e
MalSyaf <syafqz@gmail.com>
2024-02-05 09:22:00 +0800

    Posts get stored procedure

--- obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73e9eb3f883660fda31aeb2f4c8623863a792982")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c6c373cd42b7e27caabfb04edc546eb329d513e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
