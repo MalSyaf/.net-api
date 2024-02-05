3c6c373cd42b7e27caabfb04edc546eb329d513e
MalSyaf <syafqz@gmail.com>
2024-02-05 09:03:16 +0800

    Users upsert/delete stored procedures

--- Controllers/UserCompleteController.cs
+++ Controllers/UserCompleteController.cs
@@ -19,39 +19,45 @@ public class UserCompleteController : ControllerBase
     // public IActionResult Test()
     public IEnumerable<UserComplete> GetUsers(int userId, bool isActive)
     {
-        string parameters = "";
         string sql = @"EXEC TutorialAppSchema_spUsers_Get";
-        
-        if (userId != 0) 
+        string parameters = "";
+
+        if (userId != 0)
         {
-           parameters += ", @UserId = " + userId.ToString();
+            parameters += ", @UserId = " + userId.ToString();
         }
-        if (isActive) 
+        if (isActive)
         {
-           parameters += ", @Active = " + isActive.ToString();
+            parameters += ", @Active = " + isActive.ToString();
         }
 
-        sql += parameters.Substring(1);
-    
+        if (parameters.Length > 0)
+        {
+            sql += parameters.Substring(1);//, parameters.Length);
+        }
+
         IEnumerable<UserComplete> users = _dapper.LoadData<UserComplete>(sql);
         return users;
     }
 
-    [HttpPut("EditUser")]
-    public IActionResult EditUser(User user)
+    [HttpPut("UpsertUser")]
+    public IActionResult UpsertUser(UserComplete user)
     {
         string sql = @"
-            UPDATE TutorialAppSchema.Users 
-            SET [FirstName] = '" + user.FirstName + 
-                "', [LastName] = '" + user.LastName + 
-                "', [Email]= '" + user.Email + 
-                "', [Gender] = '" + user.Gender + 
-                "', [Active] = '" + user.Active + 
-            "' WHERE UserId = " + user.UserId;
+            EXEC TutorialAppSchema_spUsers_Upsert
+                 @FirstName = '" + user.FirstName +
+                "', @LastName = '" + user.LastName +
+                "', @Email = '" + user.Email +
+                "', @Gender = '" + user.Gender +
+                "', @Active = '" + user.Active +
+                "', @JobTItle = '" + user.JobTitle +
+                "', @Department = '" + user.Department +
+                "', @Salary = '" + user.Salary +
+                "', @UserId = " + user.UserId;
 
         Console.WriteLine(sql);
 
-        if(_dapper.ExecuteSql(sql))
+        if (_dapper.ExecuteSql(sql))
         {
             return Ok();
         }
@@ -59,192 +65,18 @@ public class UserCompleteController : ControllerBase
         throw new Exception("Failed to Update User");
     }
 
-    [HttpPost("AddUser")]
-    public IActionResult AddUser(UserToAddDto user)
-    {
-        string sql = @"
-        INSERT INTO TutorialAppSchema.Users(
-            [FirstName],
-            [LastName],
-            [Email],
-            [Gender],
-            [Active]
-        ) VALUES (" +
-            "'" + user.FirstName + 
-            "', '" + user.LastName +
-            "', '" + user.Email +
-            "', '" + user.Gender +
-            "', '" + user.Active +
-            "')";
-
-        Console.WriteLine(sql);
-
-        if(_dapper.ExecuteSql(sql))
-        {
-            return Ok();
-        }
-
-        throw new Exception("Failed to Add User");
-    }
-
     [HttpDelete("DeleteUser/{userId}")]
     public IActionResult DeleteUser(int userId)
     {
-        string sql = @"DELETE FROM TutorialAppSchema.Users WHERE UserId = " + userId.ToString();
+        string sql = @"TutorialAppSchema_spUsers_Delete @UserId = " + userId.ToString();
 
         Console.WriteLine(sql);
 
-        if(_dapper.ExecuteSql(sql))
+        if (_dapper.ExecuteSql(sql))
         {
             return Ok();
         }
 
         throw new Exception("Failed to Delete User");
-    }
-
-    //User Salary
-
-    [HttpGet("GetUserSalary")]
-    // public IActionResult Test()
-    public IEnumerable<UserSalary> GetUserSalary()
-    {
-        string sql = @"
-            SELECT 
-            UserSalary.UserId, 
-            UserSalary.Salary 
-            FROM TutorialAppSchema.UserSalary";
-
-        IEnumerable<UserSalary> userSalary = _dapper.LoadData<UserSalary>(sql);
-
-        return userSalary;
-    }
-
-    [HttpPut("EditUserSalary")]
-    public IActionResult EditUserSalary(UserSalary userSalary)
-    {
-        string sql = @"
-            UPDATE TutorialAppSchema.UserSalary 
-            SET [Salary] = '" + userSalary.Salary + 
-            "' WHERE UserId = " + userSalary.UserId;
-
-        Console.WriteLine(sql);
-
-        if(_dapper.ExecuteSql(sql))
-        {
-            return Ok();
-        }
-
-        throw new Exception("Failed to Update User Salary");
-    }
-
-    [HttpPost("AddUserSalary")]
-    public IActionResult AddUserSalary(UserSalary userSalary)
-    {
-        string sql = @"
-        INSERT INTO TutorialAppSchema.UserSalary(
-            [UserId],
-            [Salary]
-        ) VALUES (" + userSalary.UserId
-            + ", " + userSalary.Salary +
-            ")";
-
-        Console.WriteLine(sql);
-
-        if(_dapper.ExecuteSql(sql))
-        {
-            return Ok();
-        }
-
-        throw new Exception("Failed to Add User Salary");
-    }
-
-    [HttpDelete("DeleteUserSalary/{userId}")]
-    public IActionResult DeleteUserSalary(int userId)
-    {
-        string sql = @"DELETE FROM TutorialAppSchema.UserSalary WHERE UserId = " + userId.ToString();
-
-        Console.WriteLine(sql);
-
-        if(_dapper.ExecuteSql(sql))
-        {
-            return Ok();
-        }
-
-        throw new Exception("Failed to Delete User Salary");
-    }
-
-    //User Job Info
-
-    [HttpGet("GetUserJob")]
-    // public IActionResult Test()
-    public IEnumerable<UserJobInfo> GetUserJob()
-    {
-        string sql = @"
-            SELECT 
-            UserJobInfo.UserId,
-            UserJobInfo.JobTitle, 
-            UserJobInfo.Department 
-            FROM TutorialAppSchema.UserJobInfo";
-
-        IEnumerable<UserJobInfo> userJobInfo = _dapper.LoadData<UserJobInfo>(sql);
-
-        return userJobInfo;
-    }
-
-    [HttpPut("EditJobInfo")]
-    public IActionResult EditJobInfo(UserJobInfo userJobInfo)
-    {
-        string sql = @"
-            UPDATE TutorialAppSchema.UserJobInfo 
-            SET [JobTitle] = '" + userJobInfo.JobTitle + 
-                "', [Department] = '" + userJobInfo.Department + 
-                "' WHERE UserId = " + userJobInfo.UserId;
-
-        Console.WriteLine(sql);
-
-        if(_dapper.ExecuteSql(sql))
-        {
-            return Ok();
-        }
-
-        throw new Exception("Failed to Update User Job");
-    }
-
-    [HttpPost("AddUserJob")]
-    public IActionResult AddUserJob(UserJobInfo userJobInfo)
-    {
-        string sql = @"
-        INSERT INTO TutorialAppSchema.UserJobInfo(
-            [UserId],
-            [JobTitle],
-            [Department]
-        ) VALUES (" + userJobInfo.UserId + 
-            ", '" + userJobInfo.JobTitle +
-            "', '" + userJobInfo.Department +
-            "')";
-
-        Console.WriteLine(sql);
-
-        if(_dapper.ExecuteSql(sql))
-        {
-            return Ok();
-        }
-
-        throw new Exception("Failed to Add User Job");
-    }
-
-    [HttpDelete("DeleteUserJob/{userId}")]
-    public IActionResult DeleteUserJob(int userId)
-    {
-        string sql = @"DELETE FROM TutorialAppSchema.UserJobInfo WHERE UserId = " + userId.ToString();
-
-        Console.WriteLine(sql);
-
-        if(_dapper.ExecuteSql(sql))
-        {
-            return Ok();
-        }
-
-        throw new Exception("Failed to Delete User Job");
     }
 }
--- obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bea791932e03ec0886974a53a92c6c64ab0647d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73e9eb3f883660fda31aeb2f4c8623863a792982")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
